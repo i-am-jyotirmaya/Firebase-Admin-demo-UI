7d76efbcf1775a992e5fb666d844c3d55c796590
Jyotirmaya Sahu <sahujyotirmaya1997@gmail.com>
2021-02-15 22:20:21 +0530

    Added Users display table

--- src/components/AdminMenu/AdminMenu.tsx
+++ src/components/AdminMenu/AdminMenu.tsx
@@ -3,10 +3,20 @@ import { Box, Button, createStyles, Icon, makeStyles, Tab, Tabs, Theme, Typograp
 import { Link } from 'react-router-dom';
 import AdminMenuItem from '../AdminMenuItem/AdminMenuItem';
 import TabPanel from '../TabPanel/TabPanel';
+import MultipleUserManagement from '../UserManagement/MultipleUserManagement';
 
 const useStyles = makeStyles((theme:Theme) =>
     createStyles({
-        
+        indicator: {
+            display: 'flex',
+            justifyContent: 'center',
+            backgroundColor: 'transparent',
+            '& > span': {
+                maxWidth: 500,
+                width: '100%',
+                backgroundColor: theme.palette.primary.main
+            }
+        }
     })
 )
 
@@ -22,12 +32,12 @@ const AdminMenu: React.FC = (): JSX.Element => {
         <React.Fragment>
             {/* <AdminMenuItem icon="manage_accounts" title="List Users" to="#" />
             <AdminMenuItem icon="" title="" to="" /> */}
-            <Tabs variant="fullWidth" textColor="primary" indicatorColor="primary" value={tabValues} onChange={handleTabChange}>
+            <Tabs TabIndicatorProps={{children: <span></span>}} classes={{indicator: styles.indicator}} variant="fullWidth" textColor="primary" indicatorColor="primary" value={tabValues} onChange={handleTabChange}>
                 <Tab label="Manage multiple users" icon={<Icon>people</Icon>}/>
                 <Tab label="Manage a user" icon={<Icon>person</Icon>} />
             </Tabs>
             <TabPanel value={tabValues} index={0} >
-                All Users
+                <MultipleUserManagement />
             </TabPanel>
             <TabPanel value={tabValues} index={1} >
                 Single User
--- src/components/UserManagement/MultipleUserManagement.tsx
+++ src/components/UserManagement/MultipleUserManagement.tsx
@@ -1,9 +1,37 @@
+import { Container, createStyles, makeStyles, Table, TableHead, TableBody, TableRow, TableCell, TextField, Theme, TableContainer, Paper } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+
+        }
+    })
+);
+
 const MultipleUserManagement: React.FC = (): JSX.Element => {
+    const styles = useStyles();
+    
     return(
         <React.Fragment>
+            <Container>
+                <TextField variant="outlined" fullWidth label="Search username"/>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell></TableCell>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Email</TableCell>
+                                <TableCell>Is Admin</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
 
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
         </React.Fragment>
     );
 }
